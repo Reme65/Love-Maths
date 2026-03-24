53e2f7ffdcb2c338a0b248fb62265bc136e4d57c
Reme65 <amreme168@gmail.com>
2026-03-24 23:09:39 +0000

    repair syntax errors on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-if (gameType === "addition") {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
@@ -54,12 +54,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
 function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
+    
 }
 
 function displaySubtractQuestion() {
@@ -68,8 +68,4 @@ function displaySubtractQuestion() {
 
 function displayMultiplyQuestion() {
     
-}
-
-function displayDivisionQuestion() {
-
 }
